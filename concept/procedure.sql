b0965cace177b97ae2d6100303f9d636739ed56f
周义竣 <zhouyijun@bwcj.com>
2024-07-31 00:16:36 +0800

    feat: 存储过程带参数

--- concept/procedure.sql
+++ concept/procedure.sql
@@ -1,5 +1,16 @@
-create procedure get_clients()
-begin
-    select *
-    from clients;
-end;
+# create procedure get_clients()
+# begin
+#     select *
+#     from clients;
+# end;
+
+# 删除存储过程
+drop procedure if exists get_clientsByState;
+# 创建存储过程带参数
+# create procedure getClientsByState(state varchar(2))
+# begin
+#     select *
+#     from clients c
+#     where c.state = state;
+# end;
+
