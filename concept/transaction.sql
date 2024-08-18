29b51347d05766e5800c369639136f7349733a04
周义竣 <zhouyijun@bwcj.com>
2024-08-18 15:12:38 +0800

    feat: 事务并发

---
+++ concept/transaction.sql
@@ -0,0 +1,11 @@
+start transaction;
+
+# insert into orders(customer_id, order_date, comments, shipped_date, status)
+# values (1, '2024-04-01', 'order 1', '2019-01-02', 1);
+# insert into order_items(order_id, product_id, quantity, unit_price)
+# values (LAST_INSERT_ID(), 1, 10, 10);
+update customers
+set points = points + 100
+where customer_id = 1;
+
+commit;
